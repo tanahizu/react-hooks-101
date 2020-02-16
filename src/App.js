072773b2f66bd022501568b17717002524b672aa
田中秀和 <tanakahidekazu@tanakahidekazunoMac-mini.local>
2020-02-16 09:29:49 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount( count + 1 );
+  const decrement = () => setCount( count - 1 );
+
+  const increment2 = () => setCount( previousCount => previousCount + 1);
+  const decrement2 = () => setCount( previousCount => previousCount - 1);
+
+  const reset = () => setCount(0);
+  const double2 = () => setCount( previousCount => previousCount * 2);
+  const fizzbuzz = () => setCount( previousCount => {
+    return (previousCount % 3 === 0) ? previousCount / 3 : previousCount;
+  });
+
   return (
-    <div>
-      This is a template React app.
-    </div>
+    <React.Fragment>
+      <p>you click {count} times</p>
+      <button onClick={ increment }>
+        +1.
+      </button>
+      <button onClick={ decrement }>
+        -1.
+      </button>
+
+      <div>
+        <button onClick={ increment2 }>
+          +1.
+        </button>
+        <button onClick={ decrement2 }>
+          -1.
+        </button>
+      </div>
+
+      <div>
+        <button onClick={ reset }>reset</button>
+        <button onClick={ double2 }>x2</button>
+        <button onClick={ fizzbuzz }>3倍数の時だけ、３で割る</button>
+      </div>
+    </React.Fragment>
   );
 }
 
