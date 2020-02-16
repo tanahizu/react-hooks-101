8fe332ca89683e9f5ca60dd902fa8bb9c1ba0e64
田中秀和 <tanakahidekazu@tanakahidekazunoMac-mini.local>
2020-02-16 21:57:54 +0900

    implement component using multi-state

--- src/App.js
+++ src/App.js
@@ -1,46 +1,29 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = props => {
 
-  const increment = () => setCount( count + 1 );
-  const decrement = () => setCount( count - 1 );
+  const [name ,setName] = useState(props.name);
+  const [price ,setPrice] = useState(props.price);
 
-  const increment2 = () => setCount( previousCount => previousCount + 1);
-  const decrement2 = () => setCount( previousCount => previousCount - 1);
-
-  const reset = () => setCount(0);
-  const double2 = () => setCount( previousCount => previousCount * 2);
-  const fizzbuzz = () => setCount( previousCount => {
-    return (previousCount % 3 === 0) ? previousCount / 3 : previousCount;
-  });
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  }
 
   return (
     <React.Fragment>
-      <p>you click {count} times</p>
-      <button onClick={ increment }>
-        +1.
-      </button>
-      <button onClick={ decrement }>
-        -1.
-      </button>
-
-      <div>
-        <button onClick={ increment2 }>
-          +1.
-        </button>
-        <button onClick={ decrement2 }>
-          -1.
-        </button>
-      </div>
-
-      <div>
-        <button onClick={ reset }>reset</button>
-        <button onClick={ double2 }>x2</button>
-        <button onClick={ fizzbuzz }>3倍数の時だけ、３で割る</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={ e => setName(e.target.value)}/>
     </React.Fragment>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
